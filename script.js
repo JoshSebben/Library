38d867db98f6b68ec2effa40d09dcc48c6f3666b
JoshSebben <sebbenjosh@gmail.com>
2026-04-10 11:04:32 -0400

    Added array visualizer and working new book button

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 const myLibrary = [];
-const author = "";
-const id = 0;
-const title = "";
-const isRead = false;
+let author = "";
+let id = 0;
+let title = "";
+let isRead = false;
+let pages = 0;
 
-function Book(author, id, title, isRead) {
+function Book(author, id, title, isRead, pages) {
     this.author = author;
     this.id = id;
     this.title = title;
     this.isRead = isRead;
+    this.pages = pages;
   
     this.displayBook = function(){
         console.log("Author:" + this.author);
         console.log("ID: " + this.id);
         console.log("Title: " + this.title);
+        console.log("Is Read: " + this.isRead);
+        console.log("Pages: " + pages)
     }
 }
 
-function addBookToLibrary(author, id, title) {
+function addBookToLibrary(author, id, title, isRead, pages) {
   // take params, create a book then store it in the array
 
-  const book = new Book(author, id, title);
+  const book = new Book(author, id, title, isRead, pages);
 
   myLibrary.push(book);
 }
@@ -30,3 +34,56 @@ function displayBooks(myLibrary){
         myLibrary[i].displayBook();
     }
 }
+
+const newBook = document.getElementById("newBook");
+const array = document.getElementById("array");
+
+function addBook(){
+    let author = prompt("Enter Authors Name: ");
+    let id = prompt("Enter book ID: ");
+    let title = prompt("Enter book title: ");
+    let isRead = prompt("Is the book read: ");
+    let pages = prompt("Enter number of pages: ");
+
+    addBookToLibrary(author, id, title, isRead, pages);
+
+    const div = document.createElement("div");
+    div.innerHTML = title + ", " + id + ", " + author + ", " + isRead + ", " + pages;
+    array.appendChild(div);
+
+    const removeBookButton = document.createElement("button");
+    removeBookButton.className = "removeBook"
+    removeBookButton.innerHTML = "Remove";
+    const changeStatusButton = document.createElement("button");
+    changeStatusButton.className = "changeStatus"
+    changeStatusButton.innerHTML = "Status"
+    div.appendChild(removeBookButton);
+    div.appendChild(changeStatusButton);
+}
+const removeBookButton = document.getElementsByClassName("removeBook");
+const changeStatusButton = document.getElementsByClassName("changeStatus");
+
+function removeBook(){
+    const book = removeBookButton.parentElement;
+
+    // Select parent element, compare the inner html of it to the array database and remove if match, then delete the parent element 
+
+
+
+}
+
+function changeStatus(){
+
+    // Select parent element, change html, isRead from no to yes, yes to no
+}
+
+
+
+newBook.addEventListener('click', addBook);
+removeBookButton.addEventListener('click', removeBook);
+    
+
+
+
+
+
